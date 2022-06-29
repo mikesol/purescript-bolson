641a67e69d87a7674c161f915b3ed473ee5409c9
Verity Scheel <monoidmusician@gmail.com>
2022-06-29 13:31:42 -0400

    Constraint event monad in Always (#5)
    Thanks!

--- src/Bolson/Always.purs
+++ src/Bolson/Always.purs
@@ -5,14 +5,17 @@ import Prelude
 import Data.Monoid.Always (class Always, always)
 import Effect (Effect)
 import Heterogeneous.Mapping (class HMap, class Mapping, hmap)
+import Type.Equality (class TypeEquals)
+import Type.Proxy (Proxy)
 
-data AlwaysEffect = AlwaysEffect
+data AlwaysEffect (m :: Type -> Type) = AlwaysEffect (Proxy m)
 
 instance
-  ( Always (m Unit) (Effect Unit)
+  ( Always (m2 Unit) (Effect Unit)
+  , TypeEquals m1 m2
   ) =>
-  Mapping AlwaysEffect (i -> m Unit) (i -> Effect Unit) where
-  mapping AlwaysEffect = map always
+  Mapping (AlwaysEffect m1) (i -> m2 Unit) (i -> Effect Unit) where
+  mapping (AlwaysEffect _) = map always
 
-halways :: forall i o. HMap AlwaysEffect i o => i -> o
-halways = hmap AlwaysEffect
+halways :: forall m i o. HMap (AlwaysEffect m) i o => Proxy m -> i -> o
+halways p = hmap (AlwaysEffect p)
