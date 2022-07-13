721e9460ec4c577ef0ab5e7efd6b23cd165b2392
Verity Scheel <monoidmusician@gmail.com>
2022-07-13 12:17:09 -0400

    Allow vbusses to be recursive

--- src/Bolson/Always.purs
+++ src/Bolson/Always.purs
@@ -17,5 +17,11 @@ instance
   Mapping (AlwaysEffect m1) (i -> m2 Unit) (i -> Effect Unit) where
   mapping (AlwaysEffect _) = map always
 
+instance
+  ( HMap (AlwaysEffect m) (Record i) (Record o)
+  ) =>
+  Mapping (AlwaysEffect m) (Record i) (Record o) where
+  mapping (AlwaysEffect p) = halways p
+
 halways :: forall m i o. HMap (AlwaysEffect m) i o => Proxy m -> i -> o
 halways p = hmap (AlwaysEffect p)
