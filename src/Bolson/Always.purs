fd67abfc8cb081003af4bb62db550bba411a30ca
Mike Solomon <mike120982@gmail.com>
2022-07-25 11:32:18 +0300

    Removes type equality from Always

--- src/Bolson/Always.purs
+++ src/Bolson/Always.purs
@@ -5,23 +5,14 @@ import Prelude
 import Data.Monoid.Always (class Always, always)
 import Effect (Effect)
 import Heterogeneous.Mapping (class HMap, class Mapping, hmap)
-import Type.Equality (class TypeEquals)
-import Type.Proxy (Proxy)
 
-data AlwaysEffect (m :: Type -> Type) = AlwaysEffect (Proxy m)
+data AlwaysEffect = AlwaysEffect
 
 instance
-  ( Always (m2 Unit) (Effect Unit)
-  , TypeEquals m1 m2
+  ( Always (m Unit) (Effect Unit)
   ) =>
-  Mapping (AlwaysEffect m1) (i -> m2 Unit) (i -> Effect Unit) where
-  mapping (AlwaysEffect _) = map always
+  Mapping AlwaysEffect (i -> m Unit) (i -> Effect Unit) where
+  mapping AlwaysEffect = map always
 
-instance
-  ( HMap (AlwaysEffect m) (Record i) (Record o)
-  ) =>
-  Mapping (AlwaysEffect m) (Record i) (Record o) where
-  mapping (AlwaysEffect p) = halways p
-
-halways :: forall m i o. HMap (AlwaysEffect m) i o => Proxy m -> i -> o
-halways p = hmap (AlwaysEffect p)
+halways :: forall i o. HMap AlwaysEffect i o => i -> o
+halways = hmap AlwaysEffect
