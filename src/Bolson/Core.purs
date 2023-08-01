b4a60893d2da1ca5b892328a6f5d33528a152f2c
Mike Solomon <mike@meeshkan.com>
2023-08-01 09:40:09 +0300

    Starts on refactor

--- src/Bolson/Core.purs
+++ src/Bolson/Core.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Control.Monad.ST (ST)
 import Control.Monad.ST.Global as ST
+import Data.Bifunctor (bimap)
 import Data.Maybe (Maybe)
 import Data.Tuple (Tuple)
 import FRP.Event (Event)
@@ -16,25 +17,15 @@ type Element' interpreter r payload =
 
 newtype Element interpreter r payload = Element (Element' interpreter r payload)
 
-data Child (logic :: Type) (obj :: Type)
-  = Insert (Entity logic obj)
-  | Remove
+data Child (logic :: Type)
+  = Remove
   | Logic logic
 
-joinChild :: forall logic obj. Child logic (Entity logic obj) -> Child logic obj
-joinChild = case _ of
-  Insert a -> Insert (join a)
-  Remove -> Remove
-  Logic l -> Logic l
-
-instance Functor (Child logic) where
-  map f = case _ of
-    Insert a -> Insert (map f a)
-    Remove -> Remove
-    Logic l -> Logic l
-
 newtype DynamicChildren logic obj = DynamicChildren
-  (Event (Event (Child logic obj)))
+  ( Tuple
+      (Maybe (Tuple (Event (Child logic)) (Entity logic obj)))
+      (Event (Tuple (Event (Child logic)) (Entity logic obj)))
+  )
 
 newtype FixedChildren logic obj = FixedChildren
   (Array (Entity logic obj))
@@ -63,30 +54,30 @@ data Entity logic obj
 instance Functor (Entity logic) where
   map f = case _ of
     DynamicChildren' (DynamicChildren a) ->
-      DynamicChildren' (DynamicChildren (map (map (map f)) a))
+      DynamicChildren' (DynamicChildren (bimap (map (map (map f))) (map (map (map f))) a))
     FixedChildren' (FixedChildren a) ->
       FixedChildren' (FixedChildren (map (map f) a))
     EventfulElement' (EventfulElement a) ->
       EventfulElement' (EventfulElement (map (map f) a))
     Element' a -> Element' (f a)
 
-instance Apply (Entity logic) where
-  apply = ap
+-- instance Apply (Entity logic) where
+--   apply = ap
 
-instance Applicative (Entity logic) where
-  pure = Element'
+-- instance Applicative (Entity logic) where
+--   pure = Element'
 
-instance Bind (Entity logic) where
-  bind m f = case m of
-    DynamicChildren' (DynamicChildren a) ->
-      DynamicChildren' (DynamicChildren (map (map (joinChild <<< (map f))) a))
-    FixedChildren' (FixedChildren a) ->
-      FixedChildren' (FixedChildren (map (join <<< (map f)) a))
-    EventfulElement' (EventfulElement a) ->
-      EventfulElement' (EventfulElement (map (join <<< (map f)) a))
-    Element' a -> f a
+-- instance Bind (Entity logic) where
+--   bind m f = case m of
+--     DynamicChildren' (DynamicChildren a) ->
+--       DynamicChildren' (DynamicChildren (map (map (map f)) a))
+--     FixedChildren' (FixedChildren a) ->
+--       FixedChildren' (FixedChildren (map (join <<< (map f)) a))
+--     EventfulElement' (EventfulElement a) ->
+--       EventfulElement' (EventfulElement (map (join <<< (map f)) a))
+--     Element' a -> f a
 
-instance Monad (Entity logic)
+-- instance Monad (Entity logic)
 
 fixed
   :: forall logic obj
@@ -96,7 +87,9 @@ fixed a = FixedChildren' (FixedChildren a)
 
 dyn
   :: forall logic obj
-   . Event (Event (Child logic obj))
+   . Tuple
+       (Maybe (Tuple (Event (Child logic)) (Entity logic obj)))
+       (Event (Tuple (Event (Child logic)) (Entity logic obj)))
   -> Entity logic obj
 dyn a = DynamicChildren' (DynamicChildren a)
 
@@ -105,20 +98,3 @@ envy
    . Event (Entity logic obj)
   -> Entity logic obj
 envy a = EventfulElement' (EventfulElement a)
-
--- bussed
---   :: forall logic obj a
---    . ((a -> Effect Unit) -> Event a -> Entity logic obj)
---   -> Entity logic obj
--- bussed f = join $ Element' $ Element \psr itpt -> do
---   { event, push } <- create
---   pure $ f push event
-
--- vbussed
---   :: forall logic obj rbus bus push event
---    . RowToList bus rbus
---   => VBus rbus push event
---   => Proxy (V bus)
---   -> ({ | push } -> { | event } -> Entity logic obj)
---   -> Entity logic obj
--- vbussed px f = EventfulElement' (EventfulElement (vbus px f))
